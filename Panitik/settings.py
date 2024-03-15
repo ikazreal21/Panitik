dd454b6f1f811293bf99c1390efd43d42962481b
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2024-03-16 06:52:48 +0800

    update

--- Panitik/settings.py
+++ Panitik/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3vwqksg4$-l^ynm9hbu&)r$-_jp#(f$qv^g6cc!z(t_6_#rbm)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  ["127.0.0.1", "panitik-6cc016d1f071.herokuapp.com"]
 
 
 # Application definition
